e414812f3eb6cbc2c20b9244222748f46fd3ad7d
thiagoefonseca <thiagoefonseca2007@gmail.com>
2025-08-28 19:10:30 -0300

    Parte usuário + autenticação

---
+++ AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> Personagens { get; set; }
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+
+            _ = ObterPersonagens();
+        }
+
+        public async Task ObterPersonagens()
+        {
+            try
+            {
+                Personagens = await pService.GetPersonagensAsync();
+                OnPropertyChanged(nameof(Personagens)); //informara a view que houve carregamento
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+    }
+}
---
+++ AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Usuarios;
+using AppRpgEtec.Views.Personagens;
+using AppRpgEtec.Views.Usuarios;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        public UsuarioViewModel()
+        {
+            uService = new UsuarioService();
+            InicializarCommands();
+            
+        }
+
+        public void InicializarCommands()
+        {
+            AutenticarCommand = new Command(async() => await AutenticarUsuario());
+            RegistrarCommand = new Command(async() => await RegistrarUsuario());
+            DirecionarCadastroCommand = new Command(async () => await DirecionarParaCadastro());
+        }
+
+        private UsuarioService uService;
+        public ICommand AutenticarCommand { get; set; }
+        public ICommand RegistrarCommand { get; set; }
+        public ICommand DirecionarCadastroCommand { get; set; }
+        
+        #region Atributos e Propriedades
+
+        private string login = string.Empty;
+        private string senha = string.Empty;
+        //Ctrl + R + E para criar propriedades fácil
+        public string Login
+        {
+            get => login;
+            set
+            {
+                login = value;
+                OnPropertyChanged();
+            }
+        }  
+        public string Senha
+        { 
+            get => senha;
+            set
+            {
+                senha = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Metodos
+
+        public async Task RegistrarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = Login;
+                u.PasswordString = Senha;
+                
+                Usuario uRegistrado = await uService.PostRegistrarUsuarioAsync(u);
+
+                if(uRegistrado.Id != 0)
+                {
+                    string mensagem = $"Usuário Id {uRegistrado.Id} registrado com sucesso.";
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", mensagem, "Ok");
+
+                    await Application.Current.MainPage
+                        .Navigation.PopAsync(); //Remove a página da pilha de visualização, PushAsync() faz o oposto
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+
+        }
+        public async Task AutenticarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = login;
+                u.PasswordString = senha;
+
+                Usuario uAutenticado = await uService.PostAutenticarUsuarioAsync(u);
+
+                if (!string.IsNullOrEmpty(uAutenticado.Token))
+                {
+                    string mensagem = $"Bem vindo(a) {uAutenticado.Username}";
+                    
+                    //Guardando dados para uso futuro
+                    Preferences.Set("UsuarioId", uAutenticado.Id);
+                    Preferences.Set("UsuarioUsername", uAutenticado.Username);
+                    Preferences.Set("UsuarioPerfil", uAutenticado.Perfil);
+                    Preferences.Set("UsuarioToken", uAutenticado.Token);
+
+                    await Application.Current.MainPage
+                       .DisplayAlert("Informação", mensagem, "Ok");
+
+                    Application.Current.MainPage = new ListagemView();
+                }
+                else
+                {
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", "Dados incorretos", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + ex.InnerException, "Ok");
+            }
+        }
+
+        public async Task DirecionarParaCadastro()
+        {
+            try
+            {
+                await Application.Current.MainPage.
+                    Navigation.PushAsync(new CadastroView());
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+        #endregion
+    }
+}
